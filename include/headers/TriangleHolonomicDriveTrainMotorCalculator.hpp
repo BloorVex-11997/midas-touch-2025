22f2c8a5593a99c70cd8ea3939c2b94288b914a7
Matthew Barberson <mateo.cabral.barbosa@gmail.com>
2025-01-17 20:14:36 -0500

    Addressing issue #1 and restructuring directory
    read issue #1 for more information

--- include/headers/TriangleHolonomicDriveTrainMotorCalculator.hpp
+++ include/headers/TriangleHolonomicDriveTrainMotorCalculator.hpp
@@ -7,9 +7,9 @@
 
 Equation (in degrees) ->
 
-|m1|    | sin(theta1 + 90)   cos(theta1 + 90)    1 |   |ax|
-|m2| =  | sin(theta2 + 90)   cos(theta2 + 90)    1 | * |ay|
-|m3|    | sin(theta3 + 90)   cos(theta3 + 90)    1 |   |r |
+|m1|    | cos(theta1 + 90)   sin(theta1 + 90)    1 |   |ax|
+|m2| =  | cos(theta2 + 90)   sin(theta2 + 90)    1 | * |ay|
+|m3|    | cos(theta3 + 90)   sin(theta3 + 90)    1 |   |r |
 
 where theta_i represents the angle of a motor_i with respect
 to a line drawn through the centroid of the three wheels 
@@ -25,36 +25,39 @@ a is the vector representing the movement of the robot
 by extension ax and ay are the x and y components of the vector a
 
 r is the angular speed of the wheels 
-
 */
 
+
 //stores motor vector magnitudes
-double motorMatrix[3] = {0,0,0};
+static double motorMatrix[3] = {0.0, 0.0, 0.0};
+
+//returns a pointer to the motorMatrix, ensures that motorMatrix cannot be modified
+const double* getMotorValues(){
+    return motorMatrix; 
+}
 
 
-//matrix for calculating motor vectors
-double TransformationMatrix[3][3] = {
-    {sin(rad(THETA1 + 90.0)), cos(rad(THETA1 + 90.0)), 1},
-    {sin(rad(THETA2 + 90.0)), cos(rad(THETA2 + 90.0)), 1},
-    {sin(rad(THETA3 + 90.0)), cos(rad(THETA3 + 90.0)), 1}
-                                    };
+//base matrix for calculating motor vectors
+static double TransformationMatrix[3][3] = {
+    {cos(rad(THETA1 + 90.0)), sin(rad(THETA1 + 90.0)), 1},
+    {cos(rad(THETA2 + 90.0)), sin(rad(THETA2 + 90.0)), 1},
+    {cos(rad(THETA3 + 90.0)), sin(rad(THETA3 + 90.0)), 1}
+};
 
-
-//helper functions
 
 //updates the transformation matrix with the accumulated rotation
 void updateMatrix(int omega)
     {
-    TransformationMatrix[0][0] = sin(rad(THETA1 + 90.0 + omega));
-    TransformationMatrix[0][1] = cos(rad(THETA1 + 90.0 + omega));
-    TransformationMatrix[1][0] = sin(rad(THETA2 + 90.0 + omega));
-    TransformationMatrix[1][1] = cos(rad(THETA2 + 90.0 + omega));
-    TransformationMatrix[2][0] = sin(rad(THETA3 + 90.0 + omega));
-    TransformationMatrix[2][1] = cos(rad(THETA3 + 90.0 + omega));
+    TransformationMatrix[0][0] = cos(rad(THETA1 + 90.0 + omega));
+    TransformationMatrix[0][1] = sin(rad(THETA1 + 90.0 + omega));
+    TransformationMatrix[1][0] = cos(rad(THETA2 + 90.0 + omega));
+    TransformationMatrix[1][1] = sin(rad(THETA2 + 90.0 + omega));
+    TransformationMatrix[2][0] = cos(rad(THETA3 + 90.0 + omega));
+    TransformationMatrix[2][1] = sin(rad(THETA3 + 90.0 + omega));
     }
 
 //calculates matrix equation for m1,m2,m3
-void calculateMatrix(int ax, int ay, int r)
+void calculateMotorValues(int ax, int ay, int r)
     {
 
     int componentMatrix[3] = {ax,ay,r};
--- include/headers/globals.h
+++ include/headers/globals.h
@@ -1,10 +1,7 @@
 #pragma once
-
-
-
 //Triangle Holonomic Drive Train Motor Calculator 
 const double pi = 3.14159265358979323846;
 //degrees rotated from neutral point
 const int THETA1 = 30;
 const int THETA2 = 150;
-const int THETA3 = 270;
+const int THETA3 = 270;                         
