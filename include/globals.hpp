2b34e8b3b28b5bfd3899fd4c7d678d22f1199244
SomewhatMay <umayeer.ahsan@hotmail.com>
2025-02-01 23:02:26 -0500

    adjusted clamp port information

--- include/globals.hpp
+++ include/globals.hpp
@@ -10,7 +10,7 @@ const int DRIVETRAIN_PORT_2 = 7;
 const int DRIVETRAIN_PORT_3 = 1;
 const int ELEVATOR_PORT = 3;
 const int IMU_PORT = 15;
-const int CLAMP_PORT = 0; // TODO
+const int CLAMP_PORT = 5;  // TODO
 
 /** Voltage Limits - [-127, 127] */
 const int ABS_VOLTAGE_LIMIT = 127;
@@ -23,11 +23,11 @@ const int TURN_MULTIPLIER = 1;
 const double PRECISION_MULTIPLIER = 0.5;
 
 /** Durations */
-const int CLAMP_ACTIVE_DURATION = 1000; // in ms
+const int CLAMP_ACTIVE_DURATION = 2000;  // in ms
 
 /** Boolean Flags */
 const bool DEBUG_MODE = true;
 
 /** Robot Objects */
 inline pros::IMU imu_sensor(IMU_PORT);
-inline pros::Controller controller(pros::E_CONTROLLER_MASTER); 
+inline pros::Controller controller(pros::E_CONTROLLER_MASTER);
