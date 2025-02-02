982b93bf151222b463bc4b5287db38c3a23456c5
SomewhatMay <umayeer.ahsan@hotmail.com>
2025-02-01 19:36:05 -0500

    clamp subsystem prototyped

--- include/globals.hpp
+++ include/globals.hpp
@@ -10,18 +10,20 @@ const int DRIVETRAIN_PORT_2 = 7;
 const int DRIVETRAIN_PORT_3 = 1;
 const int ELEVATOR_PORT = 3;
 const int IMU_PORT = 15;
+const int CLAMP_PORT = 0; // TODO
 
 /** Voltage Limits - [-127, 127] */
 const int ABS_VOLTAGE_LIMIT = 127;
 const int TURN_VOLTAGE_LIMIT = 50;
 const int BELT_SPEED = 50;
+const int CLAMP_SPEED = 50;
 
 /** Multipliers */
 const int TURN_MULTIPLIER = 1;
 const double PRECISION_MULTIPLIER = 0.5;
 
 /** Durations */
-const int ELEVATOR_EXTENSION_DURATION = 1000; // in ms
+const int CLAMP_ACTIVE_DURATION = 1000; // in ms
 
 /** Boolean Flags */
 const bool DEBUG_MODE = true;
---
+++ include/subsystems/clamp/clamp.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+void clamp_periodic();
